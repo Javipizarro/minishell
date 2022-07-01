2e179595c3e72f0afc81550bc10336c6c65b2ff3
jpizarro <jpizarro@MacBook-Pro-de-USUARIO.local>
2022-07-01 12:34:05 +0200

    not valid identifiers added when export applied to the variable name

--- builtins/export.c
+++ builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: jpizarro <jpizarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:49:33 by jpizarro          #+#    #+#             */
-/*   Updated: 2022/07/01 11:37:00 by jpizarro         ###   ########.fr       */
+/*   Updated: 2022/07/01 12:31:46 by jpizarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,9 +58,9 @@ int	export_env(char *var_def, t_env **env)
 	if (var_def[0] == '=')
 		return (IDENERR);
 	while (var_def[++i] && var_def[i] != '?' && var_def[i] != '!'
-	&& var_def[i] >= 0)
+	&& var_def[i] >= 0 && var_def[i] != '=')
 		continue;
-	if (var_def[i])
+	if (var_def[i] && var_def[i] != '=')
 		return (IDENERR);
 	env_var = ft_splitonce(var_def, '=');
 	env = search_env(env_var[0], env);
